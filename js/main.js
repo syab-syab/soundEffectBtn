cdf1d02ebc79066e2973631215a6e09cbb0f5254
syab-syab <syabsyabsyabsyab@gmail.com>
2022-05-19 22:17:30 +0900

    音声再生機能追加

--- js/main.js
+++ js/main.js
@@ -1,25 +1,28 @@
 // 面倒だけどsoundフォルダ内のフォルダ名をいちいち配列に入れるしかない
-var voice1 = ["笑い声.mp3", "応！.mp3", "ざわめき.mp3", "ドンドンパフパフ.mp3", "歓声.mp3", 
-              "歓声(失敗時).mp3", "不気味.mp3", "悲鳴.mp3", "歓声と拍手.mp3", "黄色い悲鳴.mp3"];
+var voice1 = ["笑い声", "応！", "ざわめき", "ドンドンパフパフ", "歓声", 
+              "歓声(失敗時)", "不気味", "悲鳴", "歓声と拍手", "黄色い悲鳴"];
 
-var voice2 = ["犬.mp3", "牛.mp3", "ウグイス.mp3", "ウミネコ.mp3", "オオカミ.mp3", "カラス.mp3", "ゾウ.mp3", "ニワトリ.mp3",
-              "ブタの鳴き声1.mp3", "ライオン.mp3", "猫.mp3",  "馬.mp3"];
+var voice2 = ["犬", "牛", "ウグイス", "ウミネコ", "オオカミ", "カラス", "ゾウ", "ニワトリ",
+              "ブタ", "ライオン", "猫",  "馬"];
 
-var voice3 = ["ツクツクボウシ.mp3", "ヒグラシ.mp3", "ミンミンゼミ.mp3"];
+var voice3 = ["ツクツクボウシ", "ヒグラシ", "ミンミンゼミ"];
 
-var se1 = ["カーッ.mp3", "ジャジャーン.mp3", "ビシッ(高).mp3", "ピンポン1.mp3", "ピンポン2.mp3", "ピンポン3.mp3", "不正解.mp3", "突撃ラッパ.mp3", 
-          "開始のゴング.mp3", "終了のゴング.mp3", "サウナで整う.mp3", "ニュータイプ.mp3", "ビシッ.mp3",  "ピー音.mp3", "問題です.mp3"];
+var se1 = ["カーッ", "ジャジャーン", "ビシッ(高)", "ピンポン1", "ピンポン2", "ピンポン3", "不正解", "突撃ラッパ", 
+          "開始のゴング", "終了のゴング", "サウナで整う", "ニュータイプ", "ビシッ",  "ピー音", "問題です"];
 
-var se2 = ["エアーホーン.mp3", "カキーン.mp3", "チリン.mp3", "デン！.mp3", "ドラ.mp3", "ドーン.mp3", "拍手.mp3", "爆発1.mp3", "爆発2.mp3", "鳩時計.mp3",
-          "カウントダウン.mp3", "シャキーン.mp3", "テロン♪.mp3", "ドキドキ.mp3", "ドラムロール.mp3", "バーン.mp3", "ファンファーレ.mp3",  "衝撃.mp3",  "パフ.mp3",];
+var se2 = ["エアーホーン", "カキーン", "チリン", "デン！", "ドラ", "ドーン", "拍手", "爆発1", "爆発2", "鳩時計",
+          "カウントダウン", "シャキーン", "テロン♪", "ドキドキ", "ドラムロール", "バーン", "ファンファーレ",  "衝撃",  "パフ",];
 
-var se3 = ["ガブッ.mp3", "ボヨン.mp3", "ポカン.mp3", "伸びる.mp3", "切り替え1.mp3", "学校のチャイム.mp3", "恐怖.mp3", "狂気.mp3", "縮む.mp3"];
+var se3 = ["ガブッ", "ボヨン", "ポカン", "伸びる", "切り替え", "学校のチャイム", "恐怖", "狂気", "縮む"];
 
 // ボタン表示スペース
 var soundEBSpace = document.getElementById("sound-effect-button-space");
 
 // もしaudioタグのcontrolボタンでしか再生することができ無さそうなら
 // audioタグをdisplay: none;にして再生ボタンだけ押せるか試す
+
+// 表示するジャンルのタイトル
+var genreTitle = document.getElementById("genre-title");
 
 // 表示する音声ボタン
 var buttons = "";
@@ -31,7 +34,7 @@ var genre = document.getElementById("sound-select");
 
 // var btn = document.getElementById("play");
 
-// var sound = new Audio("./sound/se1/カーッ.mp3");
+// var sound = new Audio("./sound/se1/カーッ.");
 
 
 // function playTone() {
@@ -42,57 +45,90 @@ var genre = document.getElementById("sound-select");
 
 // btn.addEventListener("click", playTone);
 
+// function playSound() {
+
+// }
 
 // 再生ボタンテスト end
 
 
-// audioタグ無しでやるから、各音声ファイルのパスを配列に入れる
 // クリックされたボタンの数をに対応させて再生する
+
+// フォルダ名と再生ファイル名を格納する関数
+function soundsDirVariable(value) {
+  // 配列を返す[フォルダ名, 再生ファイル名の配列
+  switch(value) {
+    case "0":
+      return ["voice1", voice1];
+      break;
+    case "1":
+      return ["voice2", voice2];
+      break;
+    case "2":
+      return ["voice3", voice3];
+      break;
+    case "3":
+      return ["se1", se1];
+      break;
+    case "4":
+      return ["se2", se2];
+      break;
+    case "5":
+      return ["se3", se3];
+      break;
+    default:
+      // ここでちょっとエラーが起きる
+      alert(選択してください);
+  };
+}
+
+
+// 再生の関数
+function playSound() {
+  console.log(`./sound/${this.name}`);
+  var sound = new Audio(`./sound/${this.name}`);
+  sound.play();  
+}
+
+// 再生ボタンを表示させる関数
+function buttonsRegister(value) {
+  var buttonTotal = document.getElementsByClassName("sound-effect-button");
+
+  for (var i = 0; i < buttonTotal.length; i++) {
+    var fileName = `${value}/${buttonTotal[i].innerText}.mp3`
+    buttonTotal[i].addEventListener("click", {name: fileName, handleEvent: playSound});
+  }
+}
 
 
 // ここから↓を関数にまとめる
 // selectタグのchangeイベントで発動するようにする
 function indicateSoundButton() {
+  // genreTitle.innerText = "test"
   console.log(genre.innerText);
-  var soundDir;
-  switch(genre.value) {
-    case "0":
-      soundDir = "voice1";
-      soundArr = voice1;
-      break;
-    case "1":
-      soundDir = "voice2";
-      soundArr = voice2;
-      break;
-    case "2":
-      soundDir = "voice3";
-      soundArr = voice3
-      break;
-    case "3":
-      soundDir = "se1";
-      soundArr = se1;
-      break;
-    case "4":
-      soundDir = "se2";
-      soundArr = se2;
-      break;
-    case "5":
-      soundDir = "se3";
-      soundArr = se3;
-      break;
-    default:
-      alert("error!");
-  };
+  var tmp = soundsDirVariable(genre.value)
+  var soundDir = tmp[0];
+  var soundArr = tmp[1];
+  
   for (var i = 0; i < soundArr.length; i++) {
+    // 表示テキストからを消す
+    // buttons += `
+    // <div>
+    //   <button class="sound-effect-button">${soundArr[i]}</button>
+    // </div>
+    // `
     buttons += `
-    <div>
-      <button>${soundArr[i]}</button>
+    <div class="sound-effect-button">
+      ${soundArr[i]}
     </div>
     `
   }
   soundEBSpace.innerHTML = buttons;
+  buttons = "";
+  buttonsRegister(soundDir);
 }
 
-
-
 genre.addEventListener("change", indicateSoundButton);
+
+// デフォルトの状態で
+// buttonsRegister("/sound/se1/test.");
